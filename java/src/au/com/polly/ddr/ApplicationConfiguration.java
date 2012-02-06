16b390c36d5b008e2e0051a320f35fce0d84db2f
Dave Young <transitdave@gmail.com>
2012-02-06 14:15:29 +0800

    Added a lot more properties, to enable csv button in interval editor to be displayed or not, and to customize the dialogue titles and button labels associated with opening, editing and saving original and averaged csv files.

--- java/src/au/com/polly/ddr/ApplicationConfiguration.java
+++ java/src/au/com/polly/ddr/ApplicationConfiguration.java
@@ -19,9 +19,8 @@
  */
 
 package au.com.polly.ddr;
+
 import org.apache.log4j.Logger;
-
-
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
@@ -42,6 +41,15 @@ private File defaultOriginalCSVDirectory;
 private File defaultAveragedCSVDirectory;
 private String intervalAddButtonLabel;
 private String intervalDeleteButtonLabel;
+private String intervalEditorTitle;
+private String openBackgroundDataButtonLabel;
+private String editOverlayDataButtonLabel;
+private String openOverlayDataButtonLabel;
+private String saveOverlayDataButtonLabel;
+private String openBackgroundDataDialogueTitle;
+private String openOverlayDataDialogueTitle;
+private String saveOverlayDataDialogueTitle;
+private boolean displayIntervalEditorCSVButton;
 
 static {
     // default properties
@@ -51,7 +59,16 @@ static {
     defaultProperties.setProperty( "original_csv_directory", "data" );
     defaultProperties.setProperty( "averaged_csv_directory", "data" );
     defaultProperties.setProperty( "interval_add_button_label", "split" );
-    defaultProperties.setProperty( "interval_delete_button_label", "merge up" );    
+    defaultProperties.setProperty( "interval_delete_button_label", "merge up" );
+    defaultProperties.setProperty( "interval_editor_title", "averaged data interval editor" );
+    defaultProperties.setProperty( "open_background_data_button_label", "open original data" );
+    defaultProperties.setProperty( "open_background_data_dialogue_title", "open original data" );
+    defaultProperties.setProperty( "open_overlay_data_button_label", "open averaged data" );
+    defaultProperties.setProperty( "open_overlay_data_dialogue_title", "open averaged data" );
+    defaultProperties.setProperty( "edit_overlay_data_button_label", "edit averaged data" );
+    defaultProperties.setProperty( "save_overlay_data_button_label", "save averaged data" );
+    defaultProperties.setProperty( "save_overlay_data_dialogue_title", "save averaged data" );
+    defaultProperties.setProperty( "display_interval_editor_csv_button", "false" );
 }
 
 /**
@@ -206,14 +223,72 @@ protected ApplicationConfiguration( Properties properties )
     tag = "interval_add_button_label";
     if ( properties.containsKey( tag ) )
     {
-        setIntervalAddButtonLabel( properties.getProperty( tag ));
+        setIntervalAddButtonLabel( properties.getProperty( tag, defaultProperties.getProperty( tag )));
     }
 
     tag = "interval_delete_button_label";
     if ( properties.containsKey( tag ) )
     {
-        setIntervalDeleteButtonLabel(properties.getProperty(tag));
+        setIntervalDeleteButtonLabel( properties.getProperty( tag, defaultProperties.getProperty( tag ) )  );
     }
+    
+    tag = "open_background_data_button_label";
+    if ( properties.containsKey( tag ) )
+    {
+        setOpenBackgroundDataButtonLabel( properties.getProperty( tag, defaultProperties.getProperty( tag ) ) );
+    }
+    
+    tag = "open_background_dialogue_title_label";
+    if ( properties.containsKey( tag ) )
+    {
+        setOpenBackgroundDataDialogueTitle( properties.getProperty( tag, defaultProperties.getProperty( tag ) ) );
+    }
+    
+    tag = "open_overlay_data_button_label";
+    if ( properties.containsKey( tag ) )
+    {
+        setOpenOverlayDataButtonLabel( properties.getProperty( tag, defaultProperties.getProperty( tag ) ) );
+    }
+    
+    tag = "open_overlay_dialogue_title_label";
+    if ( properties.containsKey( tag ) )
+    {
+        setOpenOverlayDataDialogueTitle( properties.getProperty( tag, defaultProperties.getProperty( tag ) ) );
+    }
+    
+    tag = "save_overlay_data_button_label";
+    if ( properties.containsKey( tag ) )
+    {
+        setSaveOverlayDataButtonLabel( properties.getProperty( tag, defaultProperties.getProperty( tag ) ) );
+    }
+    
+    tag = "save_overlay_data_dialogue_title";
+    if ( properties.containsKey( tag ) )
+    {
+        setSaveOverlayDataDialogueTitle( properties.getProperty( tag, defaultProperties.getProperty( tag ) ) );
+    }
+    
+    tag = "edit_overlay_data_button_label";
+    if ( properties.containsKey( tag ) )
+    {
+        setEditOverlayDataButtonLabel( properties.getProperty( tag, defaultProperties.getProperty( tag ) ) );
+    }
+
+    tag = "interval_editor_title";
+    if ( properties.containsKey( tag ) )
+    {
+        setIntervalEditorTitle(properties.getProperty(tag, defaultProperties.getProperty(tag)));
+    }
+
+    tag = "display_interval_editor_csv_button";
+    if ( properties.containsKey( tag ) )
+    {
+        String text = properties.getProperty(tag, defaultProperties.getProperty(tag));
+        boolean flag = ( text.trim().toLowerCase().startsWith( "yes" ) ) || ( text.trim().toLowerCase().startsWith( "true" ) );
+        setDisplayIntervalEditorCSVButton( flag );
+    }
+
+
 }
 
 
@@ -267,5 +342,93 @@ protected void setIntervalDeleteButtonLabel( String label )
     this.intervalDeleteButtonLabel = label ;
 }
 
+public String getIntervalEditorTitle()
+{
+    return intervalEditorTitle;
+}
 
+protected void setIntervalEditorTitle(String intervalEditorTitle)
+{
+    this.intervalEditorTitle = intervalEditorTitle;
+}
+
+public String getOpenBackgroundDataButtonLabel()
+{
+    return openBackgroundDataButtonLabel;
+}
+
+protected void setOpenBackgroundDataButtonLabel(String openBackgroundDataButtonLabel)
+{
+    this.openBackgroundDataButtonLabel = openBackgroundDataButtonLabel;
+}
+
+public String getEditOverlayDataButtonLabel()
+{
+    return editOverlayDataButtonLabel;
+}
+
+protected void setEditOverlayDataButtonLabel(String editOverlayDataButtonLabel)
+{
+    this.editOverlayDataButtonLabel = editOverlayDataButtonLabel;
+}
+
+public String getOpenOverlayDataButtonLabel()
+{
+    return openOverlayDataButtonLabel;
+}
+
+protected void setOpenOverlayDataButtonLabel(String openOverlayDataButtonLabel)
+{
+    this.openOverlayDataButtonLabel = openOverlayDataButtonLabel;
+}
+
+public String getSaveOverlayDataButtonLabel()
+{
+    return saveOverlayDataButtonLabel;
+}
+
+protected void setSaveOverlayDataButtonLabel(String saveOverlayDataButtonLabel)
+{
+    this.saveOverlayDataButtonLabel = saveOverlayDataButtonLabel;
+}
+
+public String getOpenBackgroundDataDialogueTitle()
+{
+    return openBackgroundDataDialogueTitle;
+}
+
+protected void setOpenBackgroundDataDialogueTitle(String openBackgroundDataDialogueTitle)
+{
+    this.openBackgroundDataDialogueTitle = openBackgroundDataDialogueTitle;
+}
+
+public String getOpenOverlayDataDialogueTitle()
+{
+    return openOverlayDataDialogueTitle;
+}
+
+protected void setOpenOverlayDataDialogueTitle(String openOverlayDataDialogueTitle)
+{
+    this.openOverlayDataDialogueTitle = openOverlayDataDialogueTitle;
+}
+
+public String getSaveOverlayDataDialogueTitle()
+{
+    return saveOverlayDataDialogueTitle;
+}
+
+protected void setSaveOverlayDataDialogueTitle(String saveOverlayDataDialogueTitle)
+{
+    this.saveOverlayDataDialogueTitle = saveOverlayDataDialogueTitle;
+}
+
+public boolean isDisplayIntervalEditorCSVButton()
+{
+    return displayIntervalEditorCSVButton;
+}
+
+protected void setDisplayIntervalEditorCSVButton(boolean displayIntervalEditorCSVButton)
+{
+    this.displayIntervalEditorCSVButton = displayIntervalEditorCSVButton;
+}
 }
